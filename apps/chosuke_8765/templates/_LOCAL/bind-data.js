9896a783b233a5cf61f9b812116d464548a1a71c
m_kimura <KimuraManabu@hws000374pc.local>
2015-02-17 00:01:19 +0900

    [enhancement] エラー発生時のデバッグ情報をconsole.logへ出力

--- apps/chosuke_8765/templates/_LOCAL/bind-data.js
+++ apps/chosuke_8765/templates/_LOCAL/bind-data.js
@@ -1,42 +1,54 @@
-var _DB =
-{ "event":
-  { "name": "2015年もヤルぞ！新年会！！",
+var _DB = {
+  "event": {
+    "id": "1234567890",
+    "name": "2015年もヤルぞ！新年会！！",
     "memo": "新宿某所で開催しちゃいます！",
-    "schedule": [
-      { "date": "1/13 19:00～" },
-      { "date": "1/14 19:30 or 20:00" },
-      { "date": "1/15 19時" },
-      { "date": "2月にしてくれ！" }
-    ],
-    "entry": [
-      { "name": "たろう",
-        "comment": "がんばります！",
-        "schedule": [
-          { "reply": "2" },
-          { "reply": "2" },
-          { "reply": "2" },
-          { "reply": "2" }
-        ]
-      },
-      { "name": "はなこ",
-        "comment": "あまり飲めません",
-        "schedule": [
-          { "reply": "0" },
-          { "reply": "2" },
-          { "reply": "1" },
-          { "reply": "2" }
-        ]
-      },
-      { "name": "次郎",
-        "comment": "XSS対策<script>alert('xss');</script>",
-        "schedule": [
-          { "reply": "2" },
-          { "reply": "1" },
-          { "reply": "0" },
-          { "reply": "2" }
-        ]
-      }
-    ]
+    "schedule": [{
+      "date": "1/13 19:00～"
+    }, {
+      "date": "1/14 19:30 or 20:00"
+    }, {
+      "date": "1/15 19時"
+    }, {
+      "date": "2月にしてくれ！"
+    }],
+    "entry": [{
+      "name": "たろう",
+      "comment": "がんばります！",
+      "schedule": [{
+        "reply": "2"
+      }, {
+        "reply": "2"
+      }, {
+        "reply": "2"
+      }, {
+        "reply": "2"
+      }]
+    }, {
+      "name": "はなこ",
+      "comment": "あまり飲めません",
+      "schedule": [{
+        "reply": "0"
+      }, {
+        "reply": "2"
+      }, {
+        "reply": "1"
+      }, {
+        "reply": "2"
+      }]
+    }, {
+      "name": "次郎",
+      "comment": "XSS対策<script>alert('xss');</script>",
+      "schedule": [{
+        "reply": "2"
+      }, {
+        "reply": "1"
+      }, {
+        "reply": "0"
+      }, {
+        "reply": "2"
+      }]
+    }]
   }
 };
 var _FILE = {};
@@ -47,10 +59,10 @@ var _USR = {
     "2": "fa fa-circle text-success"
   },
   "appendClass": function(v, o) {
-    var a = (o+' '+v).split(' ');
+    var a = (o + ' ' + v).split(' ');
     var r = [];
     for (var i in a) {
-      a[i]==''?void(0):r.push(a[i]);
+      a[i] == '' ? void(0) : r.push(a[i]);
     }
     return r.join(' ');
   }
